3da60f4c2bd8a90dea5f475d5b529e02a781c587
KanHikida <coce1155@gmail.com>
2020-12-03 11:32:13 +0900

    remove comments

--- src/store/index.js
+++ src/store/index.js
@@ -6,24 +6,17 @@ Vue.use(Vuex);
 const store = new Vuex.Store({
   namespaced: true,
   state: {
-// dataと同じ
     isDialog: false
   },
   getters: {
-// 算出プロパティと同じ
   },
   mutations: {
-// stateデータを直接操作 非同期処理は定義不可能
-// データの変更は必ずここを経由する
-toggleDialog(state) {
-  state.isDialog = !state.isDialog;
-}
+    toggleDialog(state) {
+      state.isDialog = !state.isDialog;
+    }
   },
   actions: {
-// mutations の操作を各コンポーネントから呼び出すために使用する関数 非同期処理可能
   },
 })
-
-// store.commit('anything')
 
 export default store;
